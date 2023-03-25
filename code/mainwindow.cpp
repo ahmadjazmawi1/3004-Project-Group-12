40f80350e71548cadd3dad706197d09cc1ef5ca2
Ammar <you@example.com>
2023-03-25 17:41:46 -0400

    changed button names and added slots for the buttons

--- code/mainwindow.cpp
+++ code/mainwindow.cpp
@@ -6,6 +6,16 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->upButton, SIGNAL (released()), this, SLOT (upButton()));
+    connect(ui->downButton, SIGNAL (released()), this, SLOT (downButton()));
+    connect(ui->leftButton, SIGNAL (released()), this, SLOT (leftButton()));
+    connect(ui->rightButton, SIGNAL (released()), this, SLOT (rightButton()));
+    connect(ui->okButton, SIGNAL (released()), this, SLOT (okButton()));
+    connect(ui->backButton, SIGNAL (released()), this, SLOT (backButton()));
+    connect(ui->menuButton, SIGNAL (released()), this, SLOT (menuButton()));
+
+
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +23,37 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::upButton(){
+    qInfo("Up button pressed");
+
+}
+
+void MainWindow::downButton(){
+    qInfo("down button pressed");
+
+}
+
+void MainWindow::leftButton(){
+    qInfo("left button pressed");
+
+}
+
+void MainWindow::rightButton(){
+    qInfo("right button pressed");
+
+}
+
+void MainWindow::okButton(){
+    qInfo("ok button pressed");
+
+}
+
+void MainWindow::backButton(){
+    qInfo("back button pressed");
+
+}
+
+void MainWindow::menuButton(){
+    qInfo("menu button pressed");
+
+}

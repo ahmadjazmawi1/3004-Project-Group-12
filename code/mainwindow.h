4cee16d7cc7b932a8438f7bf4d0f687a38076b96
Mateen-Oyebamiji <morthaiy2003@gmail.com>
2023-03-28 17:40:17 -0400

    Implemented HRV data generator

--- code/mainwindow.h
+++ code/mainwindow.h
@@ -14,6 +14,8 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    std::map<int, int> generateData();
+
 
 private:
     Ui::MainWindow *ui;

054b9ea4d6684a6e6878548d114317a0e5db5437
ahmadjazmawi1 <ahmad_jaz@hotmail.com>
2023-04-07 08:09:51 -0400

    added HRV data and coherence values for every 5 seconds of HRV and updated graph

--- code/session.h
+++ code/session.h
@@ -27,8 +27,15 @@ public:
     QDateTime getTime();
     void setChallengeLevel(int);
     double currCoherence;
+    std::map<int, int> lowCoherenceData;    //stores HRV data for low case
 
-
+    std::map<int, int> highCoherenceData;   //stores HRV data for high case
+    QVector<double> lowCoherences = {0.509, 0.587, 0.547, 0.599, 0.594, 0.599, 0.596, 0.605, 0.680, 0.612, 0.559, 0.622};       //stores coherence values for low case
+    QVector<double> highCoherences = {0.4, 1.5, 2.9, 10.2, 11.1, 9.8, 8.8, 9.0, 9.2, 9.5, 9.8, 9.6};      //stores coherence values for high case
+    int cohIdx;     //coherence index, to keep track of whether or not to loop through the values again
+    QVector<map<int, int>> hrvData;
+    int hiOrLo;
+    QVector<QVector<double>> coherences2d;    //stores 2 vectors of coherences, one for low and one for high
 
 
 
@@ -43,7 +50,6 @@ private:
     int challengeLevel;
     QString coherenceLevel;
     QDateTime startTime;
-
 
 
 };
